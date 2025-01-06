df620ecce587248a44210a3a53980f5109c0b889
sallmayasser <sallmayasser512@gmail.com>
2025-01-06 16:17:49 +0200

    add bracket in test name regex

--- utils/validators/testValidator.js
+++ utils/validators/testValidator.js
@@ -18,8 +18,10 @@ exports.createTestValidator = [
     .withMessage("Too short Test name")
     .isLength({ max: 32 })
     .withMessage("Too long Test name")
-    .matches(/^[A-Za-z0-9\s]+$/)
-    .withMessage("test name can only contain letters, spaces and numbers")
+    .matches(/^[a-zA-Z\s\(\)\[\]]+$/)
+    .withMessage(
+      "test name can only contain letters, spaces, brackets and numbers"
+    )
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
       return true;
@@ -34,15 +36,17 @@ exports.deleteTestValidator = [
 exports.updateTestValidator = [
   check("id").isMongoId().withMessage("Invalid Test id format"),
   check("name")
-  .optional()
+    .optional()
     .notEmpty()
     .withMessage("Test name required")
     .isLength({ min: 3 })
     .withMessage("Too short Test name")
     .isLength({ max: 32 })
     .withMessage("Too long Test name")
-    .matches(/^[A-Za-z0-9\s]+$/)
-    .withMessage("test name can only contain letters, spaces and numbers")
+    .matches(/^[a-zA-Z\s\(\)\[\]]+$/)
+    .withMessage(
+      "test name can only contain letters, spaces , brackets and numbers"
+    )
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
       return true;
