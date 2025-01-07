5ccb0e95965505025fcd8ef3acc359d297c281de
sallmayasser <sallmayasser512@gmail.com>
2025-01-07 13:36:20 +0200

    prevent duplication and cascade on delete medicine / test

--- Controllers/medicineController.js
+++ Controllers/medicineController.js
@@ -1,9 +1,38 @@
+const AsyncHandler = require("express-async-handler");
 const MedicineModel = require("../Models/medicineModel");
+const PharmacyMedicineModel = require("../Models/pharmacies-medicinesModel");
+const ApiError = require("../utils/apiError");
 const factory = require("./handlerFactory");
 
 // Add a Medicine
-exports.addMedicine = factory.createOne(MedicineModel);
-exports.deleteMedicine = factory.deleteOne(MedicineModel);
+exports.addMedicine = AsyncHandler(async (req, res, next) => {
+  const { name } = req.body;
+  const exist = await MedicineModel.findOne({ name });
+  if (exist) {
+    return next(new ApiError("this tmedicineest is already exist", 409));
+  }
+  const newDoc = await factory.createOne(MedicineModel);
+
+  res.status(201).json({
+    status: "success",
+    data: newDoc,
+  });
+});
+exports.deleteMedicine = AsyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    
+  await PharmacyMedicineModel.deleteMany({ medicine: id });
+
+  const document = await MedicineModel.findByIdAndDelete(id);
+  if (!document) {
+    return next(new ApiError(`No medicine found with id ${id}`, 404));
+  }
+
+  res.status(202).json({
+    message:
+      "Medicine deleted successfully, along with any associated pharmacy.",
+  });
+});
 exports.getMedicines = factory.getAll(MedicineModel);
 exports.getMedicineById = factory.getOne(MedicineModel);
 exports.updateMedicine = factory.updateOne(MedicineModel);
--- Controllers/testController.js
+++ Controllers/testController.js
@@ -1,9 +1,37 @@
+const AsyncHandler = require("express-async-handler");
 const testModel = require("../Models/testModel");
 const factory = require("./handlerFactory");
+const ApiError = require("../utils/apiError");
+const LabTestsModel = require("../Models/labs-testsModel");
 
 // Add a Test
-exports.addTest = factory.createOne(testModel);
-exports.deleteTest = factory.deleteOne(testModel);
+exports.addTest = AsyncHandler(async (req, res, next) => {
+  const { name } = req.body;
+  const exist = await testModel.findOne({ name });
+  if (exist) {
+    return next(new ApiError("this test is already exist", 409));
+  }
+  const newDoc = await testModel.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: newDoc,
+  });
+});
+
+exports.deleteTest = AsyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  await LabTestsModel.deleteMany({ test: id });
+
+  const document = await testModel.findByIdAndDelete(id);
+  if (!document) {
+    return next(new ApiError(`No test found with id ${id}`, 404));
+  }
+
+  res.status(202).json({
+    message: "Test deleted successfully, along with any associated lab .",
+  });
+});
+
 exports.getTests = factory.getAll(testModel);
 exports.getTestById = factory.getOne(testModel);
 exports.updateTest = factory.updateOne(testModel);
